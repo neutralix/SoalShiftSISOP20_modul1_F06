d9274037f0fa218182d20a33a1c88ae13a44a2ea
babyiion <btarialiya93@gmail.com>
2020-02-19 21:53:25 +0700

    revisi 1c

--- soal1.sh
+++ soal1.sh
@@ -8,17 +8,16 @@ min_region=$(awk 'BEGIN {FS="\t"; PROCINFO["sorted_in"]="@val_num_asc"}
 echo "$min_region"
 
 echo -e "\nDua negara bagian dengan profit paling sedikit:"
-arr=$(awk -v min_region="$min_region" 'BEGIN {FS="\t";
+awk -v min_region="$min_region" 'BEGIN {FS="\t";
  PROCINFO["sorted_in"]="@val_num_asc"}
 ($13~min_region) {state[$11]+=$21}
 END{for(i in state) print i, state[i]}' Sample-Superstore.tsv |
-        awk '{print $1}' | head -2)
-echo "$arr"
+awk '{print $1}' | head -2
 
 echo -e "\nSepuluh produk dengan profit paling sedikit:"
 state1="Texas"
 state2="Illinois"
-awk -v arr="$arr" -v state1="$state1" -v state2="$state2" 'BEGIN {FS="\t"; OFS="\t";
+awk -v state1="$state1" -v state2="$state2" 'BEGIN {FS="\t"; OFS="\t";
  PROCINFO["sorted_in"]="@val_num_asc"}
 ($11~state1 || $11~state2) {produk[$17]+=$21}
 END{for(i in produk)print i,produk[i]}' Sample-Superstore.tsv |
