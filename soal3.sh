05c99e67d987aa45f3a49ae957cffdfcd7e8479f
neutralix <martin123514@yahoo.com>
2020-02-20 16:47:36 +0700

    delete folder duplicate kenangan

---
+++ soal3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+kenangancount=$(ls -l kenangan | wc -l)
+duplicatecount=$(ls -l duplicate | wc -l)
+currentcount=$(ls -l | wc -l)
+
+for ((i=1 ; $i<=28 ; i++))
+do
+	wget "https://loremflickr.com/320/240/cat" -a "wget.log" -O "pdkt_kusuma_$i"
+done
+
+for ((i=1 ; $i<=28 ; i++))
+do
+	for ((j=$((i+1)) ; $j<=28 ; j++))
+	do
+		value=$(compare -metric AE pdkt_kusuma_$i pdkt_kusuma_$j null: 2>&1)
+		if [[ $value == 0 ]]
+		then
+			mv pdkt_kusuma_$j duplicate/duplicate_$duplicatecount
+			duplicatecount=$((duplicatecount+1))
+		fi
+	done
+done
+
+for ((i=1 ; $i<=28 ; i++))
+do
+	if [[ -f pdkt_kusuma_$i ]]
+	then
+		mv pdkt_kusuma_$i kenangan/kenangan_$kenangancount
+		kenangancount=$((kenangancount+1))
+	fi
+done
+
+cp wget.log wget.log.bak
