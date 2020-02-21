a8d061b4ff8c2e0fd3c9f811e6497bda722ab1e4
babyiion <btarialiya93@gmail.com>
2020-02-21 11:03:00 +0700

    folder soal2

---
+++ Soal2/soal2_enkripsi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+newfile=`basename -s .txt $1`
+hour=$(date +"%H" -r $1)
+
+while [ $hour -gt 0 ]
+do
+	newfile=$(echo "$newfile" | tr '[A-Za-z]' '[B-ZAb-za]')
+	hour=$((hour-1))
+done
+
+mv "$1" "$newfile.txt"
